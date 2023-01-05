00bc0fa148947c2746d96584126994c6a8f716a6
Muhammad <21185758+Muhammad-Saad-01@users.noreply.github.com>
2023-01-05 08:10:00 +0200

    Initial commit

---
+++ src/main/java/com/yeshtery/pps/PhotoPublishingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yeshtery.pps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoPublishingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoPublishingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeshtery/pps/PhotoPublishingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeshtery.pps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoPublishingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
